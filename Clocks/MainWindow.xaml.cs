c42d04b56cb80cc509b364bf0cc9c39997779d39
Арсений Лихачев <ablikhachev@edu.hse.ru>
2023-02-20 18:21:51 +0500

    Add resizable analog canvas clock

--- Clocks/MainWindow.xaml.cs
+++ Clocks/MainWindow.xaml.cs
@@ -27,18 +27,74 @@ namespace Clocks
         {
             InitializeComponent();
 
-            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
             timer.Enabled = true;
         }
 
-        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+            _ = Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+              {
+                  secondHand.Angle = DateTime.Now.Second * 6;
+                  minuteHand.Angle = DateTime.Now.Minute * 6;
+                  hourHand.Angle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5);
+              }));
+        }
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            for (int i = 0; i < 60; ++i)
             {
-                secondHand.Angle = DateTime.Now.Second * 6;
-                minuteHand.Angle = DateTime.Now.Minute * 6;
-                hourHand.Angle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5);
-            }));
+                Rectangle marker = new Rectangle();
+                if (i % 5 == 0)
+                {
+                    TextBlock num = new TextBlock();
+                    int curNum = i == 0 ? 12 : i / 5;
+
+                    num.Text = curNum.ToString();
+                    num.TextAlignment = TextAlignment.Center;
+                    num.RenderTransformOrigin = new Point(1, 1);
+                    num.Foreground = Brushes.White;
+                    num.FontSize = 4;
+
+                    num.RenderTransform = new ScaleTransform(2, 2);
+
+                    double r = 34;
+                    double angle = Math.PI * curNum * 30.0 / 180.0;
+                    double x = Math.Sin(angle) * r + 50, y = -Math.Cos(angle) * r + 50;
+
+                    Canvas.SetLeft(num, x);
+                    Canvas.SetTop(num, y);
+
+                    cnv.Children.Add(num);
+
+                    marker.Width = 1.5;
+                    marker.Height = 6;
+                    marker.Fill = new SolidColorBrush(Color.FromArgb(0xe0, 0xff,
+                    0xff, 0xff));
+                    marker.Stroke = new SolidColorBrush(Color.FromArgb(0x80, 0x33,
+                    0x33, 0x33));
+                    marker.StrokeThickness = 0.5;
+                }
+                else
+                {
+                    marker.Width = 0.5;
+                    marker.Height = 3;
+                    marker.Fill = new SolidColorBrush(Color.FromArgb(0x80, 0xff,
+                    0xff, 0xff));
+                }
+                TransformGroup transforms = new TransformGroup();
+
+                transforms.Children.Add(new TranslateTransform(-(marker.Width / 2),
+                marker.Width / 2 - 40 - marker.Height));
+                transforms.Children.Add(new RotateTransform(i * 6));
+                transforms.Children.Add(new TranslateTransform(50, 50));
+
+                marker.RenderTransform = transforms;
+
+                cnv.Children.Add(marker);
+            }
         }
     }
 }
